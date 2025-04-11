f6a83b3f9f03c6b9ac935bb4588260b245daa02e
Vemy <142197561+YashVerma-251103@users.noreply.github.com>
2025-04-11 20:20:50 +0530

    initial backed structure and basic files creation.

---
+++ ams_back_end/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL','postgresql+psycopg2://username:password@localhost/mydatabase'
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
