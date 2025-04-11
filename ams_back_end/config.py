907e78179d30eb2d2a0132953b9630b0b53c5738
Vemy <142197561+YashVerma-251103@users.noreply.github.com>
2025-04-11 21:53:42 +0530

    fixed the conda issues.

--- ams_back_end/config.py
+++ ams_back_end/config.py
@@ -1,7 +1,19 @@
 import os
 
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL','postgresql+psycopg2://username:password@localhost/mydatabase'
-    )
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    # SQLALCHEMY_DATABASE_URI = os.getenv(
+    #     'DATABASE_URL','postgresql+psycopg2://username:password@localhost/mydatabase'
+    # )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+    # Configure production-level settings
